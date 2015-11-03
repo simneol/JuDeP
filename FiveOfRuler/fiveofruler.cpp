d93f82c3b6a35b5d086ef3703489f78d089aa0e4
simneol <simneol09784@gmail.com>
2015-11-04 03:21:08 +0900

    Login window & DB Connect

--- FiveOfRuler/fiveofruler.cpp
+++ FiveOfRuler/fiveofruler.cpp
@@ -1,12 +1,96 @@
 #include "fiveofruler.h"
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QVBoxLayout>
 
-FiveOfRuler::FiveOfRuler(QWidget *parent)
+#include <QtWidgets/QMessageBox>
+#include <QtSql/QSqlDataBase>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlRecord>
+
+#include <QtCore/QDebug>
+
+FiveOfRuler::FiveOfRuler(int id,QWidget *parent)
 	: QMainWindow(parent)
 {
+	QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+	db.setHostName("192.168.1.171");
+	db.setDatabaseName("fiveofruler");
+	db.setUserName("fiveofruler");
+	db.setPassword("papznye54rw5wKBy");
+
+	bool ok=db.open();
+	QSqlQuery query;
+	query.prepare("SELECT * from user_table");
 	ui.setupUi(this);
+	switch(id)
+	{
+	case 0:
+		setFixedSize(400,250);
+		idLabel=new QLabel(" Your ID ",this);
+		idLineEdit=new QLineEdit(this);
+		pwLabel=new QLabel(" Your PW ",this);
+		pwLineEdit=new QLineEdit(this);
+		pwLineEdit->setEchoMode(QLineEdit::Password);
+
+		loginButton=new QPushButton("Login",this);
+		QObject::connect(loginButton,SIGNAL(clicked()),this,SLOT(changeText()));
+
+		te =new QTextEdit(this);
+		te->setGeometry(20,100,300,100);
+		if( !query.exec() )
+			qDebug() << query.lastError();
+		else
+		{
+			qDebug("Selected!");
+			while(query.next())
+			{
+				QSqlRecord rec=query.record();
+				for( int c=0; c<rec.count(); c++ )
+					qDebug()<<query.value(c).toString();
+				te->setText(query.value(0).toString()+"kk");
+			}
+		}
+		//		te->setText(QSqlDatabase::drivers()[3]);
+		//		QObject::connect(loginButton,SIGNAL(clicked()),
+
+		/*	QHBoxLayout *idLayout=new QHBoxLayout;
+		idLayout->addWidget(idLabel);
+		idLayout->addWidget(idLineEdit);
+
+		QHBoxLayout *pwLayout=new QHBoxLayout;
+		pwLayout->addWidget(pwLabel);
+		pwLayout->addWidget(pwLineEdit);
+
+		QVBoxLayout *leftLayout=new QVBoxLayout;
+		leftLayout->addLayout(idLayout);
+		leftLayout->addLayout(pwLayout);*/
+
+		//		QVBoxLayout *topLeftLayout=new QVBoxLayout;
+
+		//		QHBoxLayout *mainLayout=new QHBoxLayout;
+		//		mainLayout->addLayout(leftLayout);
+		//		setLayout(leftLayout);
+
+		idLabel->setGeometry(20,30,100,20);
+		idLineEdit->setGeometry(120,30,100,20);
+		pwLabel->setGeometry(20,60,100,20);
+		pwLineEdit->setGeometry(120,60,100,20);
+
+		loginButton->setGeometry(230,30,70,50);
+
+		break;
+
+
+	}
 }
 
 FiveOfRuler::~FiveOfRuler()
 {
 
+}
+
+void FiveOfRuler::changeText()
+{
+	te->setText("Wow! DB is successfully loaded !");
 }
--- FiveOfRuler/fiveofruler.h
+++ FiveOfRuler/fiveofruler.h
@@ -2,6 +2,11 @@
 #define FIVEOFRULER_H
 
 #include <QtWidgets/QMainWindow>
+
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QTextEdit>
 #include "ui_fiveofruler.h"
 
 class FiveOfRuler : public QMainWindow
@@ -9,11 +14,20 @@ class FiveOfRuler : public QMainWindow
 	Q_OBJECT
 
 public:
-	FiveOfRuler(QWidget *parent = 0);
+	FiveOfRuler(int id,QWidget *parent = 0);
 	~FiveOfRuler();
 
 private:
 	Ui::FiveOfRulerClass ui;
+	QPushButton *loginButton;
+	QLabel *idLabel,*pwLabel;
+	QLineEdit *idLineEdit,*pwLineEdit;
+
+	QTextEdit *te;
+signals:
+
+	private slots:
+		void changeText();
 };
 
 #endif // FIVEOFRULER_H
