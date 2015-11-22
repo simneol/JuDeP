c4bdb95ce7e0dc4b1e70e8c9bde908ad1319c445
simneol <simneol09784@gmail.com>
2015-11-22 20:06:22 +0900

    save temp

--- FiveOfRuler/LoginWindow.cpp
+++ FiveOfRuler/LoginWindow.cpp
@@ -1,16 +1,6 @@
 #include "LoginWindow.h"
-#include "WindowManager.h"
-
-#include "RegisterDialog.h"
-#include "ForgotIdPwDialog.h"
-
-#include "FiveOfRulerDB.h"
 
 #include <QtCore/QDebug>
-
-#include <QtWidgets/QMessageBox>
-
-#include <QtSql/QSqlQuery>
 
 LoginWindow::LoginWindow(QWidget *parent)
 	: QMainWindow(parent)
@@ -18,48 +8,15 @@ LoginWindow::LoginWindow(QWidget *parent)
 	// Qt Desinger로 구성한 Ui들을 구성하는 함수
 	ui.setupUi(this);
 
-	connect(ui.loginButton,SIGNAL(clicked()),this,SLOT(login()));	// 로그인 버튼 누르면, login() 실행
-	connect(ui.registerButton,SIGNAL(clicked()),this,SLOT(createRegisterDialog()));
-	connect(ui.forgotIdPwButton,SIGNAL(clicked()),this,SLOT(createForgotIdPwDialog()));
+	connect(ui.loginButton,SIGNAL(clicked()),this,SLOT(emitLoginSignal()));	// 로그인 버튼 누르면, login() 실행
+	connect(this,SIGNAL(LoginSignal(QString,QString,bool)),&loginManager,SLOT(login(QString,QString,bool)));
+
+	connect(ui.registerButton,SIGNAL(clicked()),&loginManager,SLOT(openRegisterDialog()));
+	connect(ui.forgotIdPwButton,SIGNAL(clicked()),&loginManager,SLOT(openForgotIdPwDialog()));
 
 	connect(ui.action_Quit,SIGNAL(triggered()),this,SLOT(close()));
 }
 
 LoginWindow::~LoginWindow(){qDebug("~LoginWindow()");}
 
-void LoginWindow::login()
-{
-	QSqlQuery *query;
-	if(ui.userRadioButton->isChecked())
-		query=FiveOfRulerDB::select("user","id",ui.idLineEdit->text());
-	else
-		query=FiveOfRulerDB::select("technician","id",ui.idLineEdit->text());
-
-	if(query->value(2).toString()==ui.pwLineEdit->text()&&ui.pwLineEdit->text().length()>0)
-	{
-		qDebug("password correct");
-		if(ui.userRadioButton->isChecked())
-			WindowManager::openWindow("UserWindow");
-		else
-			WindowManager::openWindow("TechnicianWindow");
-		WindowManager::closeWindow("LoginWindow");
-	}
-	else
-	{
-		QMessageBox msgBox;
-		msgBox.setText(" Please Check Your ID/PW ");
-		msgBox.exec();
-	}
-}
-
-void LoginWindow::createRegisterDialog()
-{
-	RegisterDialog *registerDialog=new RegisterDialog;
-	registerDialog->show();
-}
-
-void LoginWindow::createForgotIdPwDialog()
-{
-	ForgotIdPwDialog *forgotIdPwDialog=new ForgotIdPwDialog;
-	forgotIdPwDialog->show();
-}
+void LoginWindow::emitLoginSignal(){emit LoginSignal(ui.idLineEdit->text(),ui.pwLineEdit->text(),ui.userRadioButton->isChecked());}
--- FiveOfRuler/WindowManager.cpp
+++ FiveOfRuler/WindowManager.cpp
@@ -17,7 +17,8 @@ void WindowManager::openWindow(QString windowName)
 	}
 	else if(windowName.compare("UserWindow")==0&&userWindow==NULL)
 	{
-		userWindow=new UserWindow;
+		if(userWindow==NULL)
+			userWindow=new UserWindow;
 		userWindow->show();
 	}
 	else if(windowName.compare("TechnicianWindow")==0&&technicianWindow==NULL)
@@ -30,9 +31,21 @@ void WindowManager::openWindow(QString windowName)
 void WindowManager::closeWindow(QString windowName)
 {
 	if(windowName.compare("LoginWindow")==0&&loginWindow!=NULL)
+	{
+		loginWindow->close();
 		delete loginWindow;
+		loginWindow=NULL;
+	//	loginWindow->hide();
+	}
 	else if(windowName.compare("UserWindow")==0&&userWindow!=NULL)
+	{
+		userWindow->close();
 		delete userWindow;
+		userWindow=NULL;
+	}
 	else if(windowName.compare("TechnicianWindow")==0&&technicianWindow!=NULL)
+	{
+		technicianWindow->close();
 		delete technicianWindow;
+	}
 }
