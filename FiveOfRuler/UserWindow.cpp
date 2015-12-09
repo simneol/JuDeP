92415c178416e39783bf2f373cf8a0bbca3e2030
name <a@a.c>
2015-12-09 14:59:10 +0900

    namechange

--- FiveOfRuler/UserWindow.cpp
+++ FiveOfRuler/UserWindow.cpp
@@ -10,8 +10,8 @@ UserWindow::UserWindow(QWidget *parent,Info* userInfo)
 	ui.setupUi(this);
 	userManager=new UserManager(userInfo);
 	connect(ui.action_Logout,SIGNAL(triggered()),userManager,SLOT(logout()));
-	connect(ui.logout,SIGNAL(clicked()),userManager,SLOT(logout()));
-	connect(ui.QnA,SIGNAL(clicked()),userManager,SLOT(openQNAWriteDialog()));
+	connect(ui.btn_Logout,SIGNAL(clicked()),userManager,SLOT(logout()));
+	connect(ui.btn_QnA,SIGNAL(clicked()),userManager,SLOT(openQNAWriteDialog()));
 }
 
 UserWindow::~UserWindow(){qDebug("~UserWindow()");}
