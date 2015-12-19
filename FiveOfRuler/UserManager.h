d4c1538c51c614c2c59071d1206ede4a9a0d3e3e
simneol <simneol09784@gmail.com>
2015-12-19 22:53:01 +0900

    temp commit

--- FiveOfRuler/UserManager.h
+++ FiveOfRuler/UserManager.h
@@ -8,11 +8,9 @@
 #include "QlistWindow.h"
 #include "RequestProduct.h"
 #include "RegisterProductInformation.h"
-#include "Message.h"
 #include "RequestShow.h"
 #include "User.h"
 
-// 사용자에 대한 기본적인 연산을 하는 클래스이다.
 class UserManager : public QObject
 {
 	Q_OBJECT
@@ -25,7 +23,6 @@ private:
 	QNAWriteDialog *qnaWriteDialog;
 	QlistWindow *qnaSeeDialog;
 	User *user;
-	Message *message;
 	RegisterProductInformation * registerProduc;
 
 	RequestProduct *requestDialog;
@@ -37,7 +34,6 @@ private:
 		void openQNASeeDialog();
 		void openRequestDialog();
 		void openRequestShowDialog();
-		void openMessageDialog();
 		void openRegisterProductInformation();
 };
 #endif
